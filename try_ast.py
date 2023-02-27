7ee60517ee6182213c51b2ab0b8dc731450ef476
Wei-Cheng Wu <wwu@isi.edu>
2023-02-27 01:19:37 -0800

    Update script

--- try_ast.py
+++ try_ast.py
@@ -1,11 +1,15 @@
 import sys
-from clang.cindex import Index
+from clang.cindex import *
 
-index = Index.create()
-translation_unit = index.read("asts/orig_asts/2mm.ast")
-print('Translation unit:', translation_unit.spelling)
+class TreeNode():
+    def __init__(self, cursor_kind=None, children=[], parent=None):
+        self.cursor_kind = cursor_kind
+        self.parent = parent
+        self.children = children
 
-def dfs_cursor(unit, cur_level):
+
+
+def dfs_cursor(cursor, cur_level):
     # raw_comment 
     #   -> None
 
@@ -23,23 +27,60 @@ def dfs_cursor(unit, cur_level):
     # objc_type_encoding 
     #  *20*0i8^{_IO_FILE=}12
 
-    # .type.kind
-    #    TypeKind.POINTER, TypeKind.FUNCTIONPROTO
+    # .kind
+
 
     # .type.kind
+    #    TypeKind.POINTER, TypeKind.FUNCTIONPROTO, CursorKind.VAR_DECL
+
+    # .type.spelling
     #   FILE, int *, etc
 
     # Basically print out the original code
     # print(" " * cur_level + "|-", " ".join([e.spelling for e in unit.get_tokens()]))
 
-    usr = str(unit.get_usr())
-    if usr.startswith("c:@SA@") or usr.startswith("c:@T@"):
+    # Remove parameters for function
+    if cursor.kind == CursorKind.PARM_DECL:
         return
 
-    print(" " * cur_level + "|-", unit.get_usr(), unit.type.spelling, unit.displayname)
-    for child in unit.get_children():
+    print(" " * cur_level + "|-", cursor.kind, cursor.spelling, cursor.displayname)
+    for child in cursor.get_children():
         dfs_cursor(child, cur_level+1)
 
 
-dfs_cursor(translation_unit.cursor, 0)
+
+def check_only_def(cursor):
+    arg_len = len(list(cursor.get_arguments()))
+    children_len = len(list(cursor.get_children()))
+    if arg_len == children_len:
+        return True
+    return False
+
+index = Index.create()
+filename = "target.c"
+
+translation_unit = index.parse("target.c")
+#translation_unit = index.read("saved_ast.txt")
+
+def create_ast_tree(translation_unit):
+    print('Translation unit:', translation_unit.spelling)
+
+    for top_node in translation_unit.cursor.get_children():
+
+        # Remove included functions
+        node_location = top_node.location.file
+        if node_location is not None and\
+                filename not in str(node_location):
+            continue
+
+        node_kind = top_node.type.kind
+        if node_kind == TypeKind.RECORD or\
+                node_kind == TypeKind.TYPEDEF or\
+                node_kind == TypeKind.FUNCTIONNOPROTO:
+            continue
+        if node_kind == TypeKind.FUNCTIONPROTO:
+            if check_only_def(top_node):
+                continue
+        dfs_cursor(top_node, 0)
+        print()
 
