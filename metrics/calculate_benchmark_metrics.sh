97b97c3175c5be03c84e160c08d1fceedc18c3e9
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2023-04-14 10:20:37 -0700

    Generate metrics for x86 decompiled code

--- metrics/calculate_benchmark_metrics.sh
+++ metrics/calculate_benchmark_metrics.sh
@@ -9,11 +9,10 @@ find ~/cs699_webassembly/emscripten_compile | grep '.c$' | xargs -I {} cp {} /tm
 cd /tmp/chstone_src
 docker run -v $(pwd):/input_files metrics python3 calculate_program_metrics.py -d . > ~/cs699_webassembly/metrics/results/original_src.json
 
-for i in 0 1 2; do
-    cd ~/cs699_webassembly/new_compiled_benchmarks/em_output_O${i}/d_wasm2c;
-    docker run -v $(pwd):/input_files metrics python3 calculate_program_metrics.py -d . > ~/cs699_webassembly/metrics/results/em_output_O${i}-d_wasm2c.json
-
-    cd ~/cs699_webassembly/new_compiled_benchmarks/em_output_O${i}/d_w2c2;
-    docker run -v $(pwd):/input_files metrics python3 calculate_program_metrics.py -d . > ~/cs699_webassembly/metrics/results/em_output_O${i}-d_w2c2.json
+for optimization_level in 0 1 2; do
+    for decompiler in wasm2c w2c2 angr ghidra snowman; do
+        cd ~/cs699_webassembly/new_compiled_benchmarks/em_output_O${optimization_level}/d_${decompiler};
+        docker run -v $(pwd):/input_files metrics python3 calculate_program_metrics.py -d . > ~/cs699_webassembly/metrics/results/em_output_O${optimization_level}-d_${decompiler}.json
+    done
 done
 
