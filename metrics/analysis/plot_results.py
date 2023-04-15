21401a7b90292a9102f3a067b5231301a595a480
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2023-04-14 19:37:09 -0700

    Plot results from all decompilers

--- metrics/analysis/plot_results.py
+++ metrics/analysis/plot_results.py
@@ -6,35 +6,30 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 
+DECOMPILERS = ["wasm2c", "w2c2", "angr", "ghidra", "snowman"]
+OPTIMIZATION_LEVELS = [0, 1, 2]
+METRICS = [
+    "Lines of code",
+    "Halstead complexity difficulty measure",
+    "McCabe cyclomatic complexity",
+    "Maximum nesting depth",
+]
+
 
 def main():
     configure_logger(logging.INFO)
 
     results_dir = Path(__file__).absolute().parent.parent / "results"
 
-    data = {
-        "original_src": json.loads((results_dir / "original_src.json").read_text()),
-        "o0_w2c2": json.loads((results_dir / "em_output_O0-d_w2c2.json").read_text()),
-        "o0_wasm2c": json.loads(
-            (results_dir / "em_output_O0-d_wasm2c.json").read_text()
-        ),
-        "o1_w2c2": json.loads((results_dir / "em_output_O1-d_w2c2.json").read_text()),
-        "o1_wasm2c": json.loads(
-            (results_dir / "em_output_O1-d_wasm2c.json").read_text()
-        ),
-        "o2_w2c2": json.loads((results_dir / "em_output_O2-d_w2c2.json").read_text()),
-        "o2_wasm2c": json.loads(
-            (results_dir / "em_output_O2-d_wasm2c.json").read_text()
-        ),
-    }
+    data = {"original_src": json.loads((results_dir / "original_src.json").read_text())}
+    for level in OPTIMIZATION_LEVELS:
+        for decompiler in DECOMPILERS:
+            data[f"o{level}_{decompiler}"] = json.loads(
+                (results_dir / f"em_output_O{level}-d_{decompiler}.json").read_text()
+            )
 
-    for metric in [
-        "Lines of code",
-        "Halstead complexity difficulty measure",
-        "McCabe cyclomatic complexity",
-        "Maximum nesting depth",
-    ]:
-        # plot_metric_averages(metric, data)
+    for metric in METRICS:
+        plot_metric_averages(metric, data)
         plot_average_metric_changes(metric, data)
 
 
@@ -81,86 +76,78 @@ def plot_metric_averages(metric, data):
     )
 
     plt.legend()
-    plt.savefig(f"average_{metric.replace(' ', '_').lower()}.png")
+    plt.savefig(f"plots/average_{metric.replace(' ', '_').lower()}.png")
 
 
 def plot_average_metric_changes(metric, data):
     plot_data = get_average_metric_changes(metric, data)
-    print(json.dumps(plot_data, indent=2))
+    # print(json.dumps(plot_data, indent=2))
 
-    bar_width = 0.25
+    bar_width = 0.15
     fig = plt.subplots()
 
-    w2c2_averages = [
-        plot_data["o0_w2c2"]["average"],
-        plot_data["o1_w2c2"]["average"],
-        plot_data["o2_w2c2"]["average"],
-    ]
-    wasm2c_averages = [
-        plot_data["o0_wasm2c"]["average"],
-        plot_data["o1_wasm2c"]["average"],
-        plot_data["o2_wasm2c"]["average"],
-    ]
-    w2c2_margins_from_average = [
-        [
-            plot_data["o0_w2c2"]["average"] - plot_data["o0_w2c2"]["min"],
-            plot_data["o1_w2c2"]["average"] - plot_data["o1_w2c2"]["min"],
-            plot_data["o2_w2c2"]["average"] - plot_data["o2_w2c2"]["min"],
-        ],
-        [
-            plot_data["o0_w2c2"]["max"] - plot_data["o0_w2c2"]["average"],
-            plot_data["o1_w2c2"]["max"] - plot_data["o1_w2c2"]["average"],
-            plot_data["o2_w2c2"]["max"] - plot_data["o2_w2c2"]["average"],
-        ],
-    ]
-    wasm2c_margins_from_average = [
-        [
-            plot_data["o0_wasm2c"]["average"] - plot_data["o0_wasm2c"]["min"],
-            plot_data["o1_wasm2c"]["average"] - plot_data["o1_wasm2c"]["min"],
-            plot_data["o2_wasm2c"]["average"] - plot_data["o2_wasm2c"]["min"],
-        ],
-        [
-            plot_data["o0_wasm2c"]["max"] - plot_data["o0_wasm2c"]["average"],
-            plot_data["o1_wasm2c"]["max"] - plot_data["o1_wasm2c"]["average"],
-            plot_data["o2_wasm2c"]["max"] - plot_data["o2_wasm2c"]["average"],
-        ],
-    ]
+    averages = dict()
+    for decompiler in DECOMPILERS:
+        averages[decompiler] = []
+        for level in OPTIMIZATION_LEVELS:
+            averages[decompiler].append(plot_data[f"o{level}_{decompiler}"]["average"])
 
-    br1 = np.arange(len(w2c2_averages))
-    br2 = [x + bar_width for x in br1]
+    margins_from_average = dict()
+    for decompiler in DECOMPILERS:
+        lower_margins = []
+        for level in OPTIMIZATION_LEVELS:
+            lower_margin = (
+                plot_data[f"o{level}_{decompiler}"]["average"]
+                - plot_data[f"o{level}_{decompiler}"]["min"]
+            )
+            lower_margins.append(lower_margin)
+        higher_margins = []
+        for level in OPTIMIZATION_LEVELS:
+            higer_margin = (
+                plot_data[f"o{level}_{decompiler}"]["max"]
+                - plot_data[f"o{level}_{decompiler}"]["average"]
+            )
+            higher_margins.append(higer_margin)
+        margins_from_average[decompiler] = [lower_margins, higher_margins]
 
-    plt.bar(
-        br1,
-        w2c2_averages,
-        yerr=w2c2_margins_from_average,
-        capsize=10,
-        color="b",
-        width=bar_width,
-        label="w2c2",
-    )
-    plt.bar(
-        br2,
-        wasm2c_averages,
-        yerr=wasm2c_margins_from_average,
-        capsize=10,
-        color="g",
-        width=bar_width,
-        label="wasm2c",
-    )
+    br_base = np.arange(len(OPTIMIZATION_LEVELS))
+    br = dict()
+    for i, decompiler in enumerate(DECOMPILERS):
+        br[decompiler] = [x + (i + 1) * bar_width for x in br_base]
+
+    labels = dict()
+    for decompiler in DECOMPILERS:
+        wasm_decompilers = {"wasm2c", "w2c2"}
+        if decompiler in wasm_decompilers:
+            labels[decompiler] = f"{decompiler} (wasm decompiler)"
+        else:
+            labels[decompiler] = f"{decompiler} (x86 decompiler)"
+
+    for decompiler in DECOMPILERS:
+        plt.bar(
+            br[decompiler],
+            averages[decompiler],
+            # yerr=margins_from_average[decompiler],
+            # capsize=2,
+            # color="b",
+            width=bar_width,
+            label=labels[decompiler],
+        )
 
     plt.title(
-        f"Average change from original code\nfor metric '{metric}'",
+        f"Average change from original code\nfor metric: '{metric}'",
         fontweight="bold",
         fontsize=15,
     )
     plt.xlabel("Optimization level", fontweight="bold", fontsize=10)
-    plt.ylabel(metric, fontweight="bold", fontsize=10)
+    plt.ylabel(f"{metric}\ndelta", fontweight="bold", fontsize=10)
     plt.xticks(
-        [r + bar_width for r in range(len(w2c2_averages))], ["-O0", "-O1", "-O2"]
+        [r + 3 * bar_width for r in range(len(OPTIMIZATION_LEVELS))],
+        ["-O0", "-O1", "-O2"],
     )
 
     plt.legend()
-    plt.savefig(f"average_delta_{metric.replace(' ', '_').lower()}.png")
+    plt.savefig(f"plots/average_delta_{metric.replace(' ', '_').lower()}.png")
 
 
 def get_average_metric_changes(metric, data):
@@ -175,11 +162,13 @@ def get_average_metric_changes(metric, data):
         )
 
         metric_deltas = []
+        num_skipped = 0
         for program in data[original_dataset]:
             if program not in data[decompiled_dataset]:
                 # logging.warning(
                 #     f"Skipping {program} since it's only in original_src dataset"
                 # )
+                num_skipped += 1
                 continue
 
             metric_delta = (
@@ -192,6 +181,10 @@ def get_average_metric_changes(metric, data):
                 logging.info(
                     f"Negative delta for {decompiled_dataset}, {program}, {metric}: {metric_delta}"
                 )
+                pass
+        logging.warning(
+            f"{num_skipped} of {len(data[original_dataset])} programs in original_src dataset skipped"
+        )
 
         average = statistics.mean(metric_deltas)
         pstdev = statistics.pstdev(metric_deltas)
