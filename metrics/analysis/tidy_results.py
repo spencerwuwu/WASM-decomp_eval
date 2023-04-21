10cead863ecf02c2acc81a422c47c1d9d132680b
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2023-04-21 11:42:29 -0700

    Normalize results for wasm2c and original src

--- metrics/analysis/tidy_results.py
+++ metrics/analysis/tidy_results.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import json
+from copy import deepcopy
+import os
+import subprocess
 
 OPTIMIZATION_LEVELS = [0, 1, 2]
 DECOMPILERS = ["wasm2c", "w2c2", "angr", "ghidra", "snowman"]
@@ -22,6 +25,13 @@ def main():
                 f"em_output_O{optimization_level}-d_{decompiler}.json"
             )
             change_hierarchy(results_file_path)
+
+    tidied1_dir = get_tidied_dir(1)
+    adapt_original_to_decompiled(tidied1_dir / "original_src.json")
+
+    copy_over_decompiled_results()
+
+    convert_wasm2c_func_names()
 
 
 # make tidied json file have the hierarchy
@@ -52,10 +62,141 @@ def change_hierarchy(results_file_path):
 
     tidied_data = recursively_sort_dict(tidied_data)
 
-    tidied_dir = get_tidied_dir()
+    tidied_dir = get_tidied_dir(1)
     tidied_dir.mkdir(exist_ok=True)
     tidied_file_path = tidied_dir / results_file_path.name
     tidied_file_path.write_text(json.dumps(tidied_data, indent=2))
+
+
+def adapt_original_to_decompiled(tidied1_original_src):
+    remove_list = [
+        "2mm.c",
+        "3mm.c",
+        "Nussinov.orig.c",
+        "chenidct.c",
+        "decode.c",
+        "huffman.c",
+        "jfif_read.c",
+        "jpeg2bmp.c",
+        "main.c",
+        "marker.c",
+        "mips.c",
+        "polybench_type1.c",
+        "polybench_type2.c",
+        "polybench_type3.c",
+        "softfloat.c",
+        "template-for-new-benchmark.c",
+    ]
+
+    d = json.loads(tidied1_original_src.read_text())
+    new_d = dict()
+    for k, v in d.items():
+        if k in remove_list:
+            continue
+        new_d[k] = v
+
+    d = new_d
+
+    tidied2_dir = get_tidied_dir(2)
+    tidied2_dir.mkdir(exist_ok=True)
+    tidied_file_path = tidied2_dir / tidied1_original_src.name
+    tidied_file_path.write_text(json.dumps(d, indent=2))
+
+    tidied2_original_src = tidied2_dir / "original_src.json"
+    move_map = {
+        "aes_dec.c": "aes.c",
+        "aes_enc.c": "aes.c",
+        "aes_func.c": "aes.c",
+        "aes_key.c": "aes.c",
+        "bf_cfb64.c": "bf.c",
+        "bf_enc.c": "bf.c",
+        "bf_skey.c": "bf.c",
+        "getbits.c": "mpeg2.c",
+        "getvlc.c": "mpeg2.c",
+        "lpc.c": "gsm.c",
+        "motion.c": "mpeg2.c",
+        "sha.c": "sha_driver.c",
+    }
+
+    new_d = deepcopy(d)
+    for k, v in d.items():
+        # if k not in new_d.keys():
+        #     new_d[k] = deepcopy(v)
+        if k in move_map.keys():
+            for func, metrics in d[k].items():
+                if func in new_d[move_map[k]]:
+                    print("CONFLICT")
+                new_d[move_map[k]][func] = metrics
+
+    d = new_d
+
+    remove_list = move_map.keys()
+    new_d = dict()
+    for k, v in d.items():
+        if k in remove_list:
+            continue
+        new_d[k] = v
+    d = new_d
+
+    tidied3_dir = get_tidied_dir(3)
+    tidied3_dir.mkdir(exist_ok=True)
+    tidied_file_path = tidied3_dir / tidied1_original_src.name
+    tidied_file_path.write_text(json.dumps(d, indent=2))
+
+
+def convert_wasm2c_func_names():
+    tidied3_dir = get_tidied_dir(3)
+
+    for optimization_level in [0, 1, 2]:
+        json_file_path = tidied3_dir / f"em_output_O{optimization_level}-d_wasm2c.json"
+
+        d = json.loads(json_file_path.read_text())
+
+        new_d = dict()
+        for file_ in d.keys():
+            new_d[file_] = dict()
+
+            file_no_ext = file_.replace(".c", "")
+            function_map_file_path = (
+                get_results_dir().parent.parent
+                / f"new_compiled_benchmarks/em_output_O{optimization_level}/d_wasm2c_symbols/{file_no_ext}.map"
+            )
+            func_map = dict()
+            for line in function_map_file_path.read_text().splitlines():
+                orig_func, wasm2c_func = line.split(":")
+                func_map[wasm2c_func] = orig_func
+
+            for metric in d[file_].keys():
+                new_d[file_][metric] = dict()
+
+                for function in d[file_][metric]:
+                    if function in func_map.keys():
+                        mapped_function = func_map[function]
+                        new_d[file_][metric][mapped_function] = d[file_][metric][
+                            function
+                        ]
+
+        d = new_d
+
+        tidied4_dir = get_tidied_dir(4)
+        tidied4_dir.mkdir(exist_ok=True)
+        tidied_file_path = tidied4_dir / json_file_path.name
+        tidied_file_path.write_text(json.dumps(d, indent=2))
+
+        subprocess.run(
+            f"cp {get_tidied_dir(3) / 'original_src.json'} {tidied4_dir}", shell=True
+        )
+
+
+def copy_over_decompiled_results():
+    tidied3_dir = get_tidied_dir(3)
+    results_dir = get_results_dir()
+
+    for f in os.listdir(results_dir):
+        if "em_output" in f:
+            src = results_dir / f
+            dst = tidied3_dir / f
+            subprocess.run(f"cp {src} {dst}", shell=True)
 
 
 def recursively_sort_dict(d):
@@ -71,8 +212,8 @@ def get_results_dir():
     return Path(__file__).parent.parent / "results"
 
 
-def get_tidied_dir():
-    return get_results_dir() / "tidied"
+def get_tidied_dir(n):
+    return get_results_dir() / f"tidied{n}"
 
 
 if __name__ == "__main__":
